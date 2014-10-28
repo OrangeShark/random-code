984e6f825b52ae3956a4dd977401b60f22398a1f
Erik Edrosa <erik.edrosa@gmail.com>
2014-10-28 19:50:16 -0400

    add some more haskell and scheme code

--- haskell/test.hs
+++ haskell/test.hs
@@ -12,4 +12,14 @@ n = a `div` length xs
        a  = 10
        xs = [1,2,3,4,5]
 
+second xs = head (tail xs)
 
+swap (x, y) = (y, x)
+
+pair x y = (x, y)
+
+palindrome xs = reverse xs == xs
+
+twice f x = f (f x)
+
+f xs = take 3 (reverse xs)
